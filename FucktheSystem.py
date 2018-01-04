d4b1994947c4c6b85d114ad70389d92d9ad3b4d4
DranelSec <austrialenard91@yahoo.com>
2018-01-05 01:00:36 +0800

    MOTHERFUCKERS
    MOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERSMOTHERFUCKERS FREE HACKING TOOLS FOR ALL MOTHERFUCKERS. SCR#pKIDDIE!

---
+++ FucktheSystem.py
@@ -0,0 +1,20 @@
+#!usr/bin/python
+import subprocess
+import time
+import os
+import webbrowser
+
+
+#IDONT WASTE MY TIME MOTHERFUCKERS
+#PLEASE AND LIEKS MOTHERFUCKERS
+# YOU LOVE ME MOTHERFUCKERS
+# WE ARE MOTHERFUCKERS
+# DIVIDED BY MOTHERFUCKERS
+# UNITED BY MOTHERFUCKERS
+# Expect us MOTHERFUCKERS
+#
+
+print ("PLEASE WAIT MOTHERFUCKERS!")
+os.system("unzip FUCKTHEZIP.zip")
+time.sleep(5)
+os.system("python fuckyou.py")
